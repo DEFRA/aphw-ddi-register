1db0464316ae13b131a8b3b99690b3720d17bdfd
sfitz42 <shaun.r.fitzsimons@gmail.com>
2023-10-24 14:32:54 +0100

    Add email to confirmation

--- app/routes/register/confirmation.js
+++ app/routes/register/confirmation.js
@@ -2,23 +2,29 @@ const sendEmail = require('../../notify')
 const { getRegister } = require('../../session/register')
 const { getDog } = require('../../session/dog')
 const { createRow } = require('../../storage')
+const { register } = require('../../constants')
 const createRegistrationNumber = require('../../create-registration-number')
 
 module.exports = {
   method: 'GET',
-  path: '/confirmation',
+  path: register.routes.confirmation,
   options: {
     handler: async (request, h) => {
       const registerDetails = {
         register: getRegister(request),
         dog: getDog(request)
       }
+
       const registrationNumber = createRegistrationNumber()
+      const email = registerDetails.register.email
       registerDetails.registrationNumber = registrationNumber
-      await sendEmail(registerDetails.register.email, { registrationNumber })
+      await sendEmail(email, { registrationNumber })
       await createRow('XL Bully', registrationNumber, registerDetails)
 
-      return h.view('confirmation', { registrationNumber })
+      return h.view(register.views.confirmation, {
+        registrationNumber,
+        email
+      })
     }
   }
 }
